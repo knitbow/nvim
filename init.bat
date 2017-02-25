282c8b0f94a68c6022f248f42565234170693b61
knitbow <upform2004@yahoo.co.jp>
2017-02-26 06:49:56 +0900

    バッチ更新

--- init.bat
+++ init.bat
@@ -2,5 +2,5 @@ setx XDG_CONFIG_HOME %AppData% -m
 setx XDG_DATA_HOME %AppData% -m
 setx XDG_CACHE_HOME %Temp% -m
 setx XDG_RUNTIME_DIR %Temp% -m
-mklink /D  %USERPROFILE%\vimfiles\ %XDG_CONFIG_HOME%\nvim
+mklink /D  %USERPROFILE%\vimfiles\ %AppData%\nvim
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\vimfiles\init.vim 
